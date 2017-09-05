b20162a9f71042fcc0cd56276f5c852d1bc69ff2
Gustav Kullberg <Gustav@Gustavs-MacBook-Pro-8.local>
2017-09-05 17:58:50 +0200

    fixed ugly vars

--- Lab1/indexer.py
+++ Lab1/indexer.py
@@ -1,15 +1,10 @@
 import sys
-
 import regex as re
-
 import pickle
-
 import os
-
 import numpy
 
 def words(text): return re.finditer(r'\p{L}+', text.lower())
-
 
 def get_files(dir, suffix):
     files = []
@@ -20,55 +15,41 @@ def get_files(dir, suffix):
 
 def main(arg):
     files = get_files(arg, 'txt')
-    master_d = dict()
-    dics = {}
-    master_counts = {}
+    dictionaries = {}
+    words_per_doc = {}
     for f in files:
         count = 0
         WORDS = (words(open(arg + '/' + f).read()))
         for w in WORDS:
-            dics.setdefault(w.group(), {}).setdefault(f, []).append(w.start())
+            dictionaries.setdefault(w.group(), {}).setdefault(f, []).append(w.start())
             count +=1
-        master_counts[f] = count
+        words_per_doc[f] = count
 
-    master_d = dict()
+    tf_idf = dict()
     default=[]
     for f in files:
-        for w in dics.keys():
-            tf = len(dics.get(w, default).get(f, default))/master_counts[f]
-            idf = numpy.log10(len(dics[w])/len(files))
-            master_d.setdefault(f, {}).setdefault(w, -tf*idf)
+        for w in dictionaries.keys():
+            tf = len(dictionaries.get(w, default).get(f, default))/words_per_doc[f]
+            idf = numpy.log10(len(dictionaries[w])/len(files))
+            tf_idf.setdefault(f, {}).setdefault(w, -tf*idf)
 
-
- #   for key in master_d.values():
-  #      for l in key.keys():
-   #         print(l + '     ' + str(key[l]))
-
-    for key in master_d.keys():
+    for key in tf_idf.keys():
         print(key)
-        for l in master_d[key].keys():
-            print(l + '     ' + str(master_d[key][l]))
+        for l in tf_idf[key].keys():
+            print(l + '     ' + str(tf_idf[key][l]))
 
     length = len(files)
     compareMatrix = numpy.zeros((length,length))
-    for i in range(0, length):
-        for j in range(0, length):
+    for i in range(length):
+        for j in range(length):
             nominator = denominator1 = denominator2 = 0
-            for k in (master_d[files[0]].keys()):
-                nominator += master_d[files[i]][k]*master_d[files[j]][k]
-                denominator1+= numpy.square(master_d[files[i]][k])
-                denominator2+= numpy.square(master_d[files[j]][k])
+            for k in (tf_idf[files[0]].keys()):
+                nominator += tf_idf[files[i]][k]*tf_idf[files[j]][k]
+                denominator1+= numpy.square(tf_idf[files[i]][k])
+                denominator2+= numpy.square(tf_idf[files[j]][k])
             denominator = numpy.sqrt(denominator1*denominator2)
             compareMatrix[i][j] = nominator/denominator
     print(compareMatrix)
-
-
-
-
-        #compareMatrix[i][j] =
-
-
-
 
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
