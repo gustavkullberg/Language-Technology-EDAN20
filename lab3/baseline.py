f1402356018b2e16ee1a1e86b8e8ca05feab0c03
Gustav Kullberg <gustavkullberg@Gustavs-MacBook-Pro.local>
2017-09-19 01:44:06 +0200

    sync

--- lab3/baseline.py
+++ lab3/baseline.py
@@ -82,8 +82,8 @@ def eval(predicted):
 
 if __name__ == '__main__':
     column_names = ['form', 'pos', 'chunk']
-    train_file = '../../corpus/conll2000/train.txt'
-    test_file = '../../corpus/conll2000/test.txt'
+    train_file = 'train.txt'
+    test_file = 'test.txt'
 
     train_corpus = conll_reader.read_sentences(train_file)
     train_corpus = conll_reader.split_rows(train_corpus, column_names)
