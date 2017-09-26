cb4223be0800032608cdc37ec406f4c5fd1a8c38
Gustav Kullberg <tpi13gku@beta-10.student.lth.se>
2017-09-26 09:45:34 +0200

    sync

--- lab4/extract.py
+++ lab4/extract.py
@@ -11,21 +11,20 @@ if __name__ == '__main__':
     subDict = {}
     for i in range(len(formatted_corpus)):
         for j in range(len(formatted_corpus[i])):
-            if(formatted_corpus[i][j]['cpostag'] == 'VV'):
-                    for k in range(len(formatted_corpus[i])):
-                            if(formatted_corpus[i][k]['deprel'] == 'SS'):
-                                verb = formatted_corpus[i][j]['form']
-                                sub = formatted_corpus[i][k]['form']
-                                verbSubPair = (verb, sub)
-                                if(verbSubPair in pairDict):
-                                    pairDict[verbSubPair] += 1
-                                    Count+=1
-                                else:
-                                    pairDict[verbSubPair] = 1
+            if(formatted_corpus[i][j]['deprel'] == 'SS'):
+                sub = formatted_corpus[i][j]['form']
+                verb = formatted_corpus[i][int(formatted_corpus[i][j]['head'])]['form']
+                verbSubPair = (verb, sub)
+                Count += 1
+                if (verbSubPair in pairDict):
+                    pairDict[verbSubPair] += 1
+                else:
+                    pairDict[verbSubPair] = 1
 
     sor = sorted(pairDict, key = pairDict.get, reverse=True)
+
 
     for i in range(5):
         print(sor[i])
     print(Count)
-    print(pairDict[('finns','som')])
+    #rint(pairDict[('finns','som')])
